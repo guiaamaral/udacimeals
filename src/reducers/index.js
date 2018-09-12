fef5c0a2c0073a868c08b82b0a3056a9d9a87d25
Guilherme Amaral <gui.larama@gmail.com>
2018-09-12 08:26:30 -0300

    Start project

---
+++ src/reducers/index.js
@@ -0,0 +1,69 @@
+import {
+  ADD_RECIPE,
+  REMOVE_FORM_CALENDAR,
+} from '../actions'
+
+const initialCalendarState = {
+  sunday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  monday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  tuesday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  wednesday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  thursday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  friday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+  saturday: {
+    breakfeast: null,
+    lunch: null,
+    dinner: null,
+  },
+}
+
+function calendar (state = initialCalendarState, action) {
+  const { day, recipe, meal } = action
+
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
+      }
+    case REMOVE_FORM_CALENDAR :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
+      }
+    default :
+      return state
+  }
+}
+
+export default calendar
